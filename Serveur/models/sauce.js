51fa1b6bfb8ae7733ac41052daea234975159f89
Giulia Sareoua <giulia.sareoua@hotmail.fr>
2021-10-21 07:23:11 +0200

    vérifications du projet, modification des champs à renseigner pour ajouter des sauces

--- Serveur/models/sauce.js
+++ Serveur/models/sauce.js
@@ -1,27 +1,44 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const validateLetters = (string) => {
+    let regex = /^[a-zA-Z0-9]+$/i;
+    if (string === "") {
+        return console.log("merci de renseigner le champ par des caractères");
+    } else {
+        return regex.test(string);
+    }
+};
+
 const sauceSchema = new Schema({
     userId: {
         type: String,
     },
     name: {
         type: String,
+        require: true,
+        validate: validateLetters,
     },
     manufacturer: {
         type: String,
+        require: true,
+        validate: validateLetters,
     },
     description: {
         type: String,
     },
     mainPepper: {
         type: String,
+        require: true,
+        validate: validateLetters,
     },
     imageUrl: {
         type: String,
     },
     heat: {
         type: Number,
+        min: 1,
+        max: 10,
     },
     likes: {
         type: Number,
